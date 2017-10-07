ebc5a2d76e4d6fa6b409e7878e28f166974d7a44
sofiamv <sofiamyrav@gmail.com>
2017-10-07 15:57:06 -0700

    FortuneCookiereturns!!!

--- FortuneCookie.java
+++ FortuneCookie.java
@@ -1,21 +1,35 @@
-package sofialeveluno;
+package sofialevelONE;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class FortuneCookie {
-	FortuneCookie bill = new FortuneCookie();
-/*Make a class, main method and showButton method. Put a printout in the showButton method. Instantiate your class and see that your printout works. 
+	// Fortune Cookie recipe: Create a button that gives one of five random
+	// fortunes when clicked.
 
-Make a JFrame in showButton method and get it to show.
+	// Make a class, main method and showButton method. Put a printout in the
+	// showButton method. Instantiate your class and see that your printout
+	// works. done
+	public static void main(String[] args) {
+		showButton();
+	}
 
-Make a JButton and add it to the JFrame.
+	static void showButton() {
+		System.out.println("yo");
+		JFrame button = new JFrame();
+		button.setVisible(true);
+		JButton realbutton = new JButton();
+		button.add(realbutton);
 
-Add an action listener to the button (auto fix the errors!!). When the button is clicked, say “Woohoo”.
+	}
+	// Make a JFrame in showButton method and get it to show. done
 
-Generate a random number between 0 and 4. Depending on the random number, show on of five fortunes.*/
-public static void main(String[] args) {
-	
-}
- void showButton(){
-	System.out.println(bill);
-	
-}
+	// Make a JButton and add it to the JFrame. done
+
+	// Add an action listener to the button (auto fix the errors!!). When the
+	// button is clicked, say “Woohoo”.
+
+	// Generate a random number between 0 and 4. Depending on the random number,
+	// show on of five fortunes.
+
 }
